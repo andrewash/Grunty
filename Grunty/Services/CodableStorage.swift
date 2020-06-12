a5c7f7b056d944c2494640421b035f33565665a9
Andrew Ash <andrew@emotiveapps.com>
2020-06-12 16:39:28 -0500

    Added classes for saving to/from the disk and common utility methods

--- Grunty/Services/CodableStorage.swift
+++ Grunty/Services/CodableStorage.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+
+/// CodableStorage allows for saving, loading, and clearing objects which implement the Encodable/Decodable to/from device storage.
+class CodableStorage {
+    /// Saves an Encodable object of type T to fileName within the caches directory
+    static func save<T:Encodable>(_ object: T, as filename: String ) {
+        let destinationURL = getCacheDirectory().appendingPathComponent(filename)
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(object)
+            if FileManager.default.fileExists(atPath: destinationURL.path) {
+                try FileManager.default.removeItem(at: destinationURL)
+            }
+            FileManager.default.createFile(atPath: destinationURL.path, contents: data, attributes: nil)
+        } catch {
+            Utilities.debugLog("Error: Cannot save object to disk")
+        }
+    }
+    
+    /// Loads a Decodable object of type T from filename within the caches directory
+    static func load<T:Decodable>(_ filename: String, as type: T.Type) -> T? {
+        let originURL = getCacheDirectory().appendingPathComponent(filename)
+        let decoder = JSONDecoder()
+        guard FileManager.default.fileExists(atPath: originURL.path) else {
+            return nil
+        }
+        do {
+            guard let data = FileManager.default.contents(atPath: originURL.path) else {
+                Utilities.debugLog("Error: Could not read contents of file \(filename) in caches directory")
+                return nil
+            }
+            let object = try decoder.decode(type, from: data)
+            return object
+        } catch {
+            Utilities.debugLog("Error: Could not decode object of type \(type) from filename \(filename)")
+            return nil
+        }
+    }
+    
+    /// Removes all documents from the cache directory
+    static func clear() {
+        let url = getCacheDirectory()
+        do {
+            let files = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [])
+            for file in files {
+                try FileManager.default.removeItem(at: file)
+            }
+        } catch {
+            Utilities.debugLog("Error: Could not remove contents of caches directory")
+        }
+    }
+    
+    // MARK: Helpers
+    static private func getCacheDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)
+        guard let cachePath = paths.first else {
+            fatalError("Error: No path found for default cache directory")
+        }
+        return cachePath
+    }
+}
---
+++ Grunty/Services/Utilities.swift
@@ -0,0 +1,18 @@
+//
+//  Utilities.swift
+//  Grunty
+//
+//  Created by Andrew Ash on 6/12/20.
+//  Copyright © 2020 Andrew Ash. All rights reserved.
+//
+
+import Foundation
+
+class Utilities {    
+    // Gate all print statements behind a debug check so we don't log insecure data in production
+    static func debugLog(_ message: String) {
+        #if DEBUG
+            print(message)
+        #endif
+    }
+}
