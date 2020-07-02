6d35cac6b70e78fa0ccdae4b53a0db8885c6a49e
Andrew Ash <andrew@emotiveapps.com>
2020-07-01 19:42:37 -0500

    Cleaner, consistent View Layout code for PostDetailsVC

--- Grunty/Controllers/PostDetailsViewController.swift
+++ Grunty/Controllers/PostDetailsViewController.swift
@@ -31,9 +31,15 @@ class PostDetailsViewController: UIViewController, ErrorReportingViewController 
         fatalError("required designated initializer for storyboards/xibs")
     }
     
+    deinit {
+        postCommentsTableView.delegate = nil
+        postCommentsTableView.dataSource = nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        prepareView()
+        view.backgroundColor = .white  // for a smooth transition when pushing this VC onto nav stack
+        layoutViews()
         loadData()
     }
 
@@ -70,75 +76,106 @@ class PostDetailsViewController: UIViewController, ErrorReportingViewController 
     // MARK: View Layout
     //==========================================================================
 
-    private let userAvatar = UIImageView(image: UIImage(named: "AvatarPlaceholder"))
-    private let userLabel = UILabel(styledWithFont: .boldSystemFont(ofSize: 20))
-    private let titleLabel = UILabel(styledWithFont: .systemFont(ofSize: 20))
-    private let bodyLabel = UILabel(styledWithFont: .systemFont(ofSize: 16))
-    private let postsByAuthor = UIButton(type: .roundedRect)
-    private let postCommentsHeading = UILabel(styledWithFont: .boldSystemFont(ofSize: 20))
-    private let postCommentsActivityIndicatorView = UIActivityIndicatorView(style: .medium)
-    private let postCommentsTableView = UITableView()
-
-    func prepareView() {
-        self.view.backgroundColor = .white  // for a smooth transition when pushing this VC onto nav stack
-        self.postCommentsTableView.register(PostCommentTableViewCell.self, forCellReuseIdentifier: PostCommentTableViewCell.identifier)
-        self.postCommentsTableView.delegate = self
-        self.postCommentsTableView.dataSource = self
-        layoutControls()
-    }
+    private let userAvatar: UIImageView = {
+        let view = UIImageView(image: UIImage(named: "AvatarPlaceholder"))
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    private let userLabel: UILabel = {
+        let label = UILabel(styledWithFont: .boldSystemFont(ofSize: 20))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let titleLabel: UILabel = {
+        let label = UILabel(styledWithFont: .systemFont(ofSize: 20))
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let bodyLabel: UILabel = {
+        let label = UILabel(styledWithFont: .systemFont(ofSize: 16))
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let postCommentsHeading: UILabel = {
+        let label = UILabel(styledWithFont: .boldSystemFont(ofSize: 20))
+        label.text = "Comments"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let postCommentsActivityIndicatorView: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView(style: .medium)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    private let postCommentsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .paleCerulean
+        tableView.register(PostCommentTableViewCell.self, forCellReuseIdentifier: PostCommentTableViewCell.identifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    private let postsByAuthor: UIButton = {
+        let button = UIButton(type: .roundedRect)
+        button.backgroundColor = UIColor.systemBlue
+        button.layer.cornerRadius = 25.0
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(goPostsByAuthor), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
-    func layoutControls() {
+    func layoutViews() {
+        // 1 - Configure UITableView
+        postCommentsTableView.delegate = self
+        postCommentsTableView.dataSource = self
+        
+        // 2 - Add views
         let subviews = [userAvatar, userLabel, titleLabel, bodyLabel, postCommentsHeading, postCommentsActivityIndicatorView, postCommentsTableView, postsByAuthor]
         for subview in subviews {
             view.addSubview(subview)
         }
-        titleLabel.numberOfLines = 2
-        bodyLabel.numberOfLines = 0
-        bodyLabel.sizeToFit()
-        postCommentsHeading.text = "Comments"
-        postCommentsTableView.backgroundColor = .paleCerulean
-        postsByAuthor.backgroundColor = UIColor.systemBlue
-        postsByAuthor.layer.cornerRadius = 25.0
-        postsByAuthor.setTitleColor(.white, for: .normal)
-        postsByAuthor.addTarget(self, action: #selector(goPostsByAuthor), for: .touchUpInside)
         
-        // Show a user avatar placeholder besides the username so it's clear the name identifies a user
-        userAvatar.translatesAutoresizingMaskIntoConstraints = false
+        // 3 - Layout
+        let hMargin: CGFloat = 25.0
+        // Layout a user avatar placeholder besides the username so it's clear the name identifies a user
         NSLayoutConstraint.activate([
-            userAvatar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 25.0),
+            userAvatar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: hMargin),
             userAvatar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 7.0),
             userAvatar.widthAnchor.constraint(equalToConstant: 50.0),
             userAvatar.heightAnchor.constraint(equalTo: userAvatar.widthAnchor)
         ])
-        userLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             userLabel.leadingAnchor.constraint(equalTo: userAvatar.trailingAnchor, constant: 8.0),
             userLabel.centerYAnchor.constraint(equalTo: userAvatar.centerYAnchor),
-            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: userLabel.trailingAnchor, constant: 25.0),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: userLabel.trailingAnchor, constant: hMargin),
             userLabel.heightAnchor.constraint(equalToConstant: 30.0)
         ])
-
-        NSLayoutConstraint.activate(Utilities.makeStandardPhoneConstraints(forView: titleLabel,
-                                                                           previousView: userLabel,
-                                                                           rootView: self.view,
-                                                                           topSpacing: 10.0,
-                                                                           height: 28.0 * CGFloat(titleLabel.numberOfLines)))
-        NSLayoutConstraint.activate(Utilities.makeStandardPhoneConstraints(forView: bodyLabel,
-                                                                           previousView: titleLabel,
-                                                                           rootView: self.view,
-                                                                           topSpacing: 4.0,
-                                                                           height: 120.0))
-
-        // Show a comments heading with an activity indicator to the right when comments are loading
-        postCommentsHeading.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Layout title and body with custom vertical spacing
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 25.0),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: hMargin),
+            titleLabel.topAnchor.constraint(equalTo: userLabel.bottomAnchor, constant: 10.0),
+            titleLabel.heightAnchor.constraint(equalToConstant: 28.0 * CGFloat(titleLabel.numberOfLines))
+        ])
+        NSLayoutConstraint.activate([
+            bodyLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: hMargin),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: bodyLabel.trailingAnchor, constant: hMargin),
+            bodyLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4.0),
+            bodyLabel.heightAnchor.constraint(equalToConstant: 120.0)
+        ])
+        
+        // Layout a comments heading with an activity indicator to the right when comments are loading
         NSLayoutConstraint.activate([
             postCommentsHeading.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 25.0),
             postCommentsHeading.topAnchor.constraint(equalTo: bodyLabel.bottomAnchor, constant: 32.0),
             postCommentsHeading.widthAnchor.constraint(equalToConstant: 120.0),
             postCommentsHeading.heightAnchor.constraint(equalToConstant: 30.0)
         ])
-
-        postCommentsActivityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             postCommentsActivityIndicatorView.leadingAnchor.constraint(equalTo: postCommentsHeading.trailingAnchor, constant: 0.0),
             postCommentsActivityIndicatorView.centerYAnchor.constraint(equalTo: postCommentsHeading.centerYAnchor),
@@ -146,16 +183,14 @@ class PostDetailsViewController: UIViewController, ErrorReportingViewController 
             postCommentsActivityIndicatorView.heightAnchor.constraint(equalTo: postCommentsActivityIndicatorView.widthAnchor)
         ])
 
-        // Comments appear in a UITableView in the middle, with no fixed height
-        postCommentsTableView.translatesAutoresizingMaskIntoConstraints = false
+        // Comments are laid out in a UITableView in the middle, with no fixed height
         NSLayoutConstraint.activate([
             postCommentsTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: postCommentsTableView.trailingAnchor, constant: 20),
             postCommentsTableView.topAnchor.constraint(equalTo: postCommentsHeading.bottomAnchor, constant: 4)
         ])
 
-        // Posts By Author appears at bottom of the view
-        postsByAuthor.translatesAutoresizingMaskIntoConstraints = false
+        // Layout Posts By Author at bottom of the view
         NSLayoutConstraint.activate([
             postsByAuthor.topAnchor.constraint(equalTo: postCommentsTableView.bottomAnchor, constant: 15),
             postsByAuthor.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
